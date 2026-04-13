8c38bb6c129fb143f8c8071e9dee115f7acfc500
Rikuuzy <shandhyyudha@gmail.com>
2026-04-13 23:14:34 +0700

    Asersi

---
+++ Pertemuan6/Asersi/src/ExceptionOnArray.java
@@ -0,0 +1,19 @@
+/* Nama File    : ExceptionOnArray.java
+ * Deskripsi    : Program penggunaan eksepsi library Java
+ * Pembuat      : Hadrian Shandhy Yudha
+ * Tanggal      : 13 April 2026
+ */
+public class ExceptionOnArray {
+    public static void main(String[] args) {
+        // instansiasi object array integer
+        Integer[] arrayInteger = new Integer[4];
+        try {
+            arrayInteger[2] = 11;
+            arrayInteger[4] = 10;
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            exception.printStackTrace();
+        } finally {
+            System.out.println("clean up code...");
+        }
+    }
+}
